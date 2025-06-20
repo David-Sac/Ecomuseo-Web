78fc53f36418d06313d8c3b9f807cf020979c92b
David Sac <saccsaradavid@gmail.com>
2025-06-20 13:02:46 -0500

    Mejorando_welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -114,29 +114,43 @@
                                 referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
+                <!-- ↓↓↓ CARRUSEL DINÁMICO DE COMPONENTES ↓↓↓ -->
                 <div class="contenedor">
-                <!-- Carousel de Imágenes -->
                     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                        <ol class="carousel-indicators">
-                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                        </ol>
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img class="d-block w-100" src="../images/welcome/carrousel_1.jpg" alt="First slide">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="d-block w-100" src="../images/welcome/Photo_Agro.jpg" alt="Second slide">
+                    
+                    {{-- 1) Indicadores --}}
+                    <ol class="carousel-indicators">
+                        @foreach($components as $i => $component)
+                        <li data-target="#carouselExampleIndicators"
+                            data-slide-to="{{ $i }}"
+                            class="{{ $i === 0 ? 'active' : '' }}"></li>
+                        @endforeach
+                    </ol>
+                    
+                    {{-- 2) Slides --}}
+                    <div class="carousel-inner">
+                        @foreach($components as $i => $component)
+                        <div class="carousel-item {{ $i === 0 ? 'active' : '' }}">
+                            <img class="d-block w-100"
+                                src="{{ asset($component->rutaImagenComponente) }}"
+                                alt="{{ $component->titleComponente }}">
+                            <div class="carousel-caption d-none d-md-block">
+                            <h5>{{ $component->titleComponente }}</h5>
+                            <p>{{ $component->description }}</p>
                             </div>
                         </div>
-                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
+                        @endforeach
+                    </div>
+
+                    {{-- 3) Controles --}}
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Anterior</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Siguiente</span>
+                    </a>
                     </div>
                 </div>
             </div>
